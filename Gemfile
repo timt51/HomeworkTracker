55f785696da2495554627ef486dc13370d5cc11d
Timothy Truong, Jr <timt51@live.com>
2013-08-12 16:06:14 -0400

    Can login with facebook

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 gem "paperclip"
 gem 'omniauth-twitter'
+gem 'omniauth-facebook'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ HomeworkTracker::Application.routes.draw do
 
   match '/auth/:provider/callback', to: 'sessions#create', via: :get
   match "/login" => redirect("/auth/twitter"), as: :login, via: :get
+  match "/login1" => redirect("/auth/facebook"), as: :login1, via: :get
   match "/logout" => "sessions#destroy", as: :logout, via: :get
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
